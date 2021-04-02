18670f86832c4dbee151966ee27f77788e8541e8
bigdatawiki <bigdatawiki.india@gmail.com>
2021-04-02 17:53:42 +0530

    updating test_student.py

--- test_student.py
+++ test_student.py
@@ -1,29 +1,30 @@
 from student import StudentDB
 import pytest
 
-db = None
+
+@pytest.fixture
+def db():
+    print("Inside db function")
+    stud_db = StudentDB()
+    stud_db.connect('data.json')
+    return stud_db
 
 
-def setup_module(module):
-    print("Inside setup_module function")
-    global db
-    db = StudentDB()
-    db.connect('data.json')
-
-
+'''
 def teardown_module(module):
     print("Inside teardown_module function")
     db.close()
+'''
 
 
-def test_scott_data():
+def test_scott_data(db):
     scott_data = db.get_data('Scott')
     assert scott_data['id'] == 1
     assert scott_data['name'] == 'Scott'
     assert scott_data['result'] == 'pass'
 
 
-def test_mark_data():
+def test_mark_data(db):
     mark_data = db.get_data('Mark')
     assert mark_data['id'] == 2
     assert mark_data['name'] == 'Mark'
